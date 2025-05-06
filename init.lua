d2224edf981ab24f9880829903a426adf9b1b913
Cole Carley <cole.carley@gmail.com>
2025-05-05 21:17:13 -0700

    git, comments, snippets, lspsaga, and more!

--- init.lua
+++ init.lua
@@ -97,7 +97,31 @@ lazy.setup({
 	{ "hrsh7th/nvim-cmp" },
 	{ "stevearc/conform.nvim" },
 	{ "nvim-tree/nvim-tree.lua" },
-	{ "okuuva/auto-save.nvim" },
+	{
+		"okuuva/auto-save.nvim",
+		version = "^1.0.0", -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
+		cmd = "ASToggle", -- optional for lazy loading on command
+		event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
+		opts = {
+			enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+			trigger_events = { -- See :h events
+				immediate_save = { "BufLeave", "FocusLost", "QuitPre", "VimSuspend" }, -- vim events that trigger an immediate save
+				defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
+				cancel_deferred_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+			},
+			-- function that takes the buffer handle and determines whether to save the current buffer or not
+			-- return true: if buffer is ok to be saved
+			-- return false: if it's not ok to be saved
+			-- if set to `nil` then no specific condition is applied
+			condition = nil,
+			write_all_buffers = false, -- write all buffers when the current one meets `condition`
+			noautocmd = false, -- do not execute autocmds when saving
+			lockmarks = false, -- lock marks when saving, see `:h lockmarks` for more details
+			debounce_delay = 1000, -- delay after which a pending save is executed
+			-- log debug messages to 'auto-save.log' file in neovim cache directory, set to `true` to enable
+			debug = false,
+		},
+	},
 	{
 		"toppair/peek.nvim",
 		event = { "VeryLazy" },
@@ -123,13 +147,6 @@ lazy.setup({
 			mappings = { basic = true },
 		},
 	},
-	{
-		"lervag/vimtex",
-		lazy = false,
-		init = function()
-			vim.g.vimtex_view_method = "zathura"
-		end,
-	},
 	{ "akinsho/toggleterm.nvim", version = "*", opts = {} },
 	{ "tpope/vim-fugitive" },
 	{ "rafamadriz/friendly-snippets" },
@@ -140,6 +157,28 @@ lazy.setup({
 		opts = {
 			version = "v2.*",
 			build = "make install_jsregexp",
+		},
+	},
+	{
+		"nvimdev/lspsaga.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter", -- optional
+			"nvim-tree/nvim-web-devicons", -- optional
+		},
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- add any options here
+		},
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			"rcarriga/nvim-notify",
 		},
 	},
 })
@@ -249,3 +288,64 @@ function _G.open_next_terminal()
 end
 
 vim.api.nvim_set_keymap("t", "<leader>\\", "<cmd>lua open_next_terminal()<CR>", { noremap = true, silent = true })
+
+local group = vim.api.nvim_create_augroup("autosave", {})
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "AutoSaveWritePost",
+	group = group,
+	callback = function(opts)
+		if opts.data.saved_buffer ~= nil then
+			local filename = vim.api.nvim_buf_get_name(opts.data.saved_buffer)
+			vim.notify("AutoSave: saved " .. filename .. " at " .. vim.fn.strftime("%H:%M:%S"), vim.log.levels.INFO)
+		end
+	end,
+})
+
+require("lspsaga").setup({
+	lightbulb = {
+		enable = false,
+	},
+	symbol_in_winbar = {
+		enable = true,
+		separator = " › ",
+		hide_keyword = false,
+		show_file = true,
+		folder_level = 1,
+		color_mode = true,
+		delay = 300,
+	},
+	border = "single",
+	devicon = true,
+	title = true,
+	expand = "⊞",
+	collapse = "⊟",
+	code_action = "💡",
+	actionfix = " ",
+	lines = { "┗", "┣", "┃", "━", "┏" },
+	kind = {},
+	imp_sign = "󰳛 ",
+})
+
+require("noice").setup({
+	lsp = {
+		-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+		override = {
+			["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+			["vim.lsp.util.stylize_markdown"] = true,
+			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+		},
+	},
+	-- you can enable a preset for easier configuration
+	presets = {
+		bottom_search = true, -- use a classic bottom cmdline for search
+		command_palette = true, -- position the cmdline and popupmenu together
+		long_message_to_split = true, -- long messages will be sent to a split
+		inc_rename = false, -- enables an input dialog for inc-rename.nvim
+		lsp_doc_border = false, -- add a border to hover docs and signature help
+	},
+})
+
+vim.keymap.set("n", "<leader>ki", "<cmd>Lspsaga hover_doc<cr>")
+vim.keymap.set("n", "<leader>r", "<cmd>Lspsaga rename<cr>")
+vim.keymap.set("n", "<leader>]", "<cmd>vertical rightbelow split<cr><cmd>Lspsaga goto_definition<cr>")
