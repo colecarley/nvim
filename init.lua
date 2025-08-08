b4e6a60b6985a1801b36f0ebe8bd62e708c05ae6
Cole Carley <cole.carley@gmail.com>
2025-08-08 14:13:40 -0700

    remove barbar

--- init.lua
+++ init.lua
@@ -171,22 +171,6 @@ lazy.setup({
 		-- dependencies = { "echasnovski/mini.icons" },
 		opts = {},
 	},
-	{
-		"romgrk/barbar.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		init = function()
-			vim.g.barbar_auto_setup = false
-		end,
-		opts = {
-			-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-			-- animation = true,
-			-- insert_at_start = true,
-			-- …etc.
-		},
-		version = "^1.0.0", -- optional: only update when a new 1.x version is released
-	},
 })
 
 require("toggleterm").setup({})
